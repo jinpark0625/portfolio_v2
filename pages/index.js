47eb256d71cd10d66b3ea4517d0389ac2f985b29
jin park <jinpark0625@gmail.com>
2022-08-23 18:17:25 -0400

    [ADD]add react-spring

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,13 @@ import {
   Text,
   Sparkles,
   Text3D,
+  Center,
+  useCursor,
 } from "@react-three/drei";
 import * as random from "maath/random";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
 import Poppins from "../public/fonts/Poppins_Bold.json";
+import Inter from "../public/fonts/Inter_Bold.json";
 import * as THREE from "three";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import { extend } from "@react-three/fiber";
@@ -22,43 +25,17 @@ import DatGui, { DatColor, DatNumber } from "react-dat-gui";
 import { Vector3 } from "three";
 import { MeshSurfaceSampler, OBJLoader } from "three-stdlib";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
+import { useSpring, animated, config } from "@react-spring/three";
 
 const vertices = [];
 const tempPosition = new Vector3();
 const count = 10000;
 let points;
 
-const Star = ({
-  starsOpt,
-  scale = Array.from({ length: 50 }, () => 0.5 + Math.random() * 2),
-}) => {
-  //load text
-  const font = useLoader(FontLoader, "/fonts/Poppins_Bold.json");
-  extend({ TextGeometry });
-  const fontConfig = useMemo(() => {
-    const fontConfig = { font, size: 0.2, height: 0.2 };
-    return fontConfig;
-  }, []);
-
+const Star = ({ starsOpt }) => {
   // refs
   const ref = useRef();
-  const materialRef = useRef();
-  const fontRef = useRef();
   const textRef = useRef();
-
-  //make 1000 spheres by using random
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(1000), { radius: 1.5 })
-  );
-
-  // const [{ box, goo, final }] = useState(() => {
-  //   const box = random.inBox(new Float32Array(10000), { sides: [1, 2, 1] });
-  //   const goo = random.inSphere(box.slice(0), { radius: 0.75 });
-
-  //   const final = box.slice(0); // final buffer that will be used for the points mesh
-
-  //   return { box, goo, final };
-  // });
 
   //animation
   // useFrame((state, delta) => {
@@ -70,7 +47,7 @@ const Star = ({
   const groupRef = useRef();
 
   useLayoutEffect(() => {
-    console.log(ref);
+    textRef.current.letterSpacing = 3;
     const textSample = new MeshSurfaceSampler(textRef.current);
     textSample.build();
 
@@ -78,7 +55,7 @@ const Star = ({
     for (let i = 0; i < count; i++) {
       textSample.sample(tempPosition);
       vertices.push(
-        (Math.random() - 0.5) * 0.01 + tempPosition.x,
+        (Math.random() - 0.5) * 0.04 + tempPosition.x,
         (Math.random() - 0.5) * 0.02 + tempPosition.y,
         0
       );
@@ -88,25 +65,16 @@ const Star = ({
       "position",
       new THREE.BufferAttribute(points, 3)
     );
-
-    ref.current.geometry.computeBoundingBox();
-    const center = ref.current.geometry.boundingBox.getCenter(new Vector3());
-
-    ref.current.position.x = -center.x;
-    ref.current.position.y = -center.y;
   }, []);
 
   return (
-    <>
-      <group ref={groupRef}>
-        <mesh>
-          <planeGeometry />
-        </mesh>
+    <Center>
+      <animated.group ref={groupRef}>
         <Text3D
-          letterSpacing={-0.1}
           size={1}
+          letterSpacing={2}
           position={[0, 0, 0]}
-          font={Poppins}
+          font={Inter}
           ref={textRef}
           visible={false}
         >
@@ -124,8 +92,8 @@ const Star = ({
             // vertexColors={true}
           />
         </points>
-      </group>
-    </>
+      </animated.group>
+    </Center>
   );
 };
 
@@ -133,6 +101,15 @@ export default function Home({ results }) {
   const [windowSize, setWindowSize] = useState({
     width: null,
     height: null,
+  });
+
+  const positionRef = useRef({
+    mouseX: 0,
+    mouseY: 0,
+    destinationX: 0,
+    destinationY: 0,
+    distanceX: 0,
+    distanceY: 0,
   });
 
   useEffect(() => {
@@ -152,6 +129,15 @@ export default function Home({ results }) {
       handleResize();
     });
 
+    // window.addEventListener("mousemove", (e) => {
+    //   const { clientX, clientY } = e;
+
+    //   positionRef.current.mouseX = (clientX / window.innerWidth) * 2 - 1;
+    //   positionRef.current.mouseY = -(clientY / window.innerHeight) * 2 + 1;
+    //   // positionRef.current.mouseX = clientX / window.innerWidth - 0.5;
+    //   // positionRef.current.mouseY = clientY / window.innerHeight - 0.5;
+    // });
+
     return window.removeEventListener("resize", () => {
       handleResize();
     });
@@ -161,6 +147,19 @@ export default function Home({ results }) {
     starSize: 0.005,
     starColor: "white",
     background: "black",
+  });
+
+  const [hovered, setHover] = useState(false);
+
+  const [props, set] = useSpring(() => ({
+    pos: [0, 0, 0],
+  }));
+
+  console.log(props);
+
+  const { hoverValue } = useSpring({
+    hoverValue: hovered ? 1 : 0,
+    config: config.molasses,
   });
 
   return (
@@ -184,8 +183,22 @@ export default function Home({ results }) {
         dpr={[1, 2]}
       >
         <OrbitControls makeDefault />
-        {/* <ambientLight intensity={0.5} /> */}
-        <Star starsOpt={starsOpt} />
+
+        <Star
+          starsOpt={starsOpt}
+          onMouseMove={({ clientX, clientY }) => {
+            const x = (clientX / window.innerWidth) * 2 - 1;
+            const y = -(clientY / window.innerHeight) * 2 + 1;
+
+            set({
+              pos: [x, y, 0],
+            });
+          }}
+        />
+        <axesHelper
+          position={[0, 0, 0]}
+          onUpdate={(self) => self.setColors("#ff2080", "#20ff80", "#2080ff")}
+        />
       </Canvas>
     </div>
   );
