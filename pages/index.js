26bec2224525cfd7756ce017bf46bef30611cdd0
jin park <jinpark0625@gmail.com>
2022-08-25 23:54:16 -0400

    [ADD]Add interaction with particles

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState, useEffect, useRef, useMemo, useLayoutEffect } from "react";
 import Seo from "../components/seo";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
 import {
   Point,
   Points,
@@ -22,27 +22,40 @@ import * as THREE from "three";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import { extend } from "@react-three/fiber";
 import DatGui, { DatColor, DatNumber } from "react-dat-gui";
-import { Vector3 } from "three";
+import { Vector2, Vector3 } from "three";
 import { MeshSurfaceSampler, OBJLoader } from "three-stdlib";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 import { useSpring, animated, config } from "@react-spring/three";
+import { vertexShader, fragmentShader } from "./shader";
 
-const vertices = [];
-const tempPosition = new Vector3();
-const count = 10000;
-let points;
+const raycaster = new THREE.Raycaster();
+let pointsValue;
 
-const Star = ({ starsOpt }) => {
+const Star = ({ starsOpt, pos, width, height }) => {
   // refs
   const ref = useRef();
   const textRef = useRef();
+  const planeRef = useRef();
+
+  const { viewport } = useThree();
+  const { gl } = useThree();
+
+  const count = 15000;
 
   //animation
-  // useFrame((state, delta) => {
-  //   ref.current.rotation.x -= delta / 10;
-  //   ref.current.rotation.y -= delta / 15;
-  // });
-  // rotation={[0, 0, Math.PI / 4]}
+  useFrame(({ mouse, camera, clock }) => {
+    ref.current.material.uniforms.uTime.value = clock.getElapsedTime();
+
+    const x = (mouse.x * viewport.width) / 2;
+    const y = (mouse.y * viewport.height) / 2;
+
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects([planeRef.current]);
+
+    if (intersects[0]) {
+      ref.current.material.uniforms.uMouse.value = new Vector2(x, y);
+    }
+  });
 
   const groupRef = useRef();
 
@@ -52,48 +65,94 @@ const Star = ({ starsOpt }) => {
     textSample.build();
 
     const tempPosition = new Vector3();
-    for (let i = 0; i < count; i++) {
+
+    const vertices = new Float32Array(count * 3);
+    const scale = new Float32Array(count);
+    const indices = new Uint16Array(count);
+    const angle = new Float32Array(count);
+
+    for (let i = 0, j = 0; i < count; i++) {
+      const i3 = i * 3;
       textSample.sample(tempPosition);
-      vertices.push(
-        (Math.random() - 0.5) * 0.04 + tempPosition.x,
-        (Math.random() - 0.5) * 0.02 + tempPosition.y,
-        0
-      );
+
+      vertices[i3 + 0] = (Math.random() - 0.5) * 0.04 + tempPosition.x;
+      vertices[i3 + 1] = (Math.random() - 0.5) * 0.02 + tempPosition.y;
+      vertices[i3 + 2] = 0;
+
+      scale[i] = Math.random() * 1.2;
+
+      angle[j] = Math.random() * Math.PI;
+
+      indices[j] = i;
+      j++;
     }
-    points = new Float32Array(vertices);
+
     ref.current.geometry.setAttribute(
       "position",
-      new THREE.BufferAttribute(points, 3)
+      new THREE.BufferAttribute(vertices, 3)
+    );
+    ref.current.geometry.setAttribute(
+      "aScale",
+      new THREE.BufferAttribute(scale, 1)
+    );
+    ref.current.geometry.setAttribute(
+      "angle",
+      new THREE.BufferAttribute(angle, 1)
+    );
+    ref.current.geometry.setAttribute(
+      "pindex",
+      new THREE.BufferAttribute(indices, 1)
     );
   }, []);
+  // * gl.getPixelRatio()
+  const uniforms = useMemo(
+    () => ({
+      uMouse: {
+        value: new Vector2(0, 0),
+      },
+      uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+      uSize: {
+        value: 1.5,
+      },
+      uTime: {
+        value: 0,
+      },
+      uColor: { value: new THREE.Color(0x0f85d6) },
+    }),
+    []
+  );
 
   return (
-    <Center>
-      <animated.group ref={groupRef}>
-        <Text3D
-          size={1}
-          letterSpacing={2}
-          position={[0, 0, 0]}
-          font={Inter}
-          ref={textRef}
-          visible={false}
-        >
-          Test
-          <meshStandardMaterial color="white" />
-        </Text3D>
-
-        <points ref={ref}>
-          <pointsMaterial
-            attach="material"
-            color={starsOpt.starColor}
-            size={starsOpt.starSize}
-            alphaTest={0.2}
-            // map={colorMap}
-            // vertexColors={true}
-          />
-        </points>
-      </animated.group>
-    </Center>
+    <>
+      <mesh ref={planeRef} position={[0, 0, 0]} visible={false}>
+        <planeGeometry args={[5, 3, 2, 2]} />
+        <meshBasicMaterial wireframe={true} />
+      </mesh>
+      <Center>
+        <animated.group ref={groupRef}>
+          <Text3D
+            size={1}
+            letterSpacing={2}
+            position={[0, 0, 0]}
+            font={Inter}
+            ref={textRef}
+            visible={false}
+          >
+            Hello.
+            <meshStandardMaterial color="white" />
+          </Text3D>
+          <points ref={ref}>
+            <shaderMaterial
+              uniforms={uniforms}
+              vertexShader={vertexShader}
+              fragmentShader={fragmentShader}
+              depthWrite={false}
+              transparent={true}
+            />
+          </points>
+        </animated.group>
+      </Center>
+    </>
   );
 };
 
@@ -102,6 +161,8 @@ export default function Home({ results }) {
     width: null,
     height: null,
   });
+
+  const canvasRef = useRef();
 
   const positionRef = useRef({
     mouseX: 0,
@@ -129,15 +190,6 @@ export default function Home({ results }) {
       handleResize();
     });
 
-    // window.addEventListener("mousemove", (e) => {
-    //   const { clientX, clientY } = e;
-
-    //   positionRef.current.mouseX = (clientX / window.innerWidth) * 2 - 1;
-    //   positionRef.current.mouseY = -(clientY / window.innerHeight) * 2 + 1;
-    //   // positionRef.current.mouseX = clientX / window.innerWidth - 0.5;
-    //   // positionRef.current.mouseY = clientY / window.innerHeight - 0.5;
-    // });
-
     return window.removeEventListener("resize", () => {
       handleResize();
     });
@@ -146,21 +198,14 @@ export default function Home({ results }) {
   const [starsOpt, setStarsOpt] = useState({
     starSize: 0.005,
     starColor: "white",
-    background: "black",
+    background: "#17191c",
   });
-
-  const [hovered, setHover] = useState(false);
 
   const [props, set] = useSpring(() => ({
-    pos: [0, 0, 0],
+    pos: { x: 0, y: 0, z: 0 },
   }));
 
-  console.log(props);
-
-  const { hoverValue } = useSpring({
-    hoverValue: hovered ? 1 : 0,
-    config: config.molasses,
-  });
+  const radius = 0.2;
 
   return (
     <div
@@ -169,9 +214,18 @@ export default function Home({ results }) {
         height: windowSize.height,
         background: starsOpt.background,
       }}
+      onMouseMove={({ clientX, clientY }) => {
+        const x = (clientX / window.innerWidth) * 2 - 1;
+        const y = -(clientY / window.innerHeight) * 2 + 1;
+
+        set({
+          pos: { x: x, y: y, z: 0 },
+        });
+      }}
     >
       <Seo title="Home" />
       <Canvas
+        ref={canvasRef}
         camera={{
           fov: 75,
           near: 0.1,
@@ -183,21 +237,12 @@ export default function Home({ results }) {
         dpr={[1, 2]}
       >
         <OrbitControls makeDefault />
-
-        <Star
-          starsOpt={starsOpt}
-          onMouseMove={({ clientX, clientY }) => {
-            const x = (clientX / window.innerWidth) * 2 - 1;
-            const y = -(clientY / window.innerHeight) * 2 + 1;
-
-            set({
-              pos: [x, y, 0],
-            });
-          }}
-        />
+        <Star starsOpt={starsOpt} pos={props} />
         <axesHelper
           position={[0, 0, 0]}
           onUpdate={(self) => self.setColors("#ff2080", "#20ff80", "#2080ff")}
+          width={windowSize.width}
+          height={windowSize.height}
         />
       </Canvas>
     </div>
