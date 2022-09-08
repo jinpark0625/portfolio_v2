9ddafc41da7da204a35b4805dd10236933b2bfca
jin park <jinpark0625@gmail.com>
2022-09-08 00:23:16 -0400

    add page transition

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,49 @@
 import Navbar from "./navBar";
+import { useSpring, animated, config, useTransition } from "@react-spring/web";
+import { withRouter, useRouter } from "next/router";
+import { createContext, useEffect, useState } from "react";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, router }) => {
+  const transitions = useTransition(router, {
+    from: { opacity: 0, transform: "translate3d(100%,0,0)" },
+    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+    // onRest: () => set(!show),
+  });
+
+  // console.log(router);
+
+  // useEffect(() => {
+  //   if (router) {
+  //     const handleRouteChange = (url, { shallow }) => {
+  //       console.log(
+  //         `App is changing to ${url} ${
+  //           shallow ? "with" : "without"
+  //         } shallow routing`
+  //       );
+  //     };
+
+  //     router.events.on("routeChangeStart", handleRouteChange);
+
+  //     // If the component is unmounted, unsubscribe
+  //     // from the event with the `off` method:
+  //     return () => {
+  //       console.log("Router Listener Unmounted");
+  //       router.events.off("routeChangeStart", handleRouteChange);
+  //     };
+  //   }
+  // }, []);
+
   return (
     <>
       <Navbar />
-      <div>{children}</div>
+      {/* <div>{children}</div> */}
+      {transitions(
+        (style, item) =>
+          item && <animated.div style={style}>{children}</animated.div>
+      )}
     </>
   );
 };
 
-export default Layout;
+export default withRouter(Layout);
