810029946495b3b9560a2ccbb9ca92fedc39139a
jin park <jinpark0625@gmail.com>
2022-09-08 23:34:55 -0400

    add theme

---
+++ styles/theme.js
@@ -0,0 +1,40 @@
+const fontSize = {
+  categoryNum: 0.625,
+  footerSize: 0.813,
+  paragraph: 1,
+  tagline: 1.125,
+  subTitle_m: 1.5,
+  subTitme: 1.875,
+  headerIntro: 2.125,
+  title_m: 2.75,
+  title: 3.125,
+};
+const space = [12, 20, 30, 35, 54, 60, 75, 100, 105, 145];
+const color = {
+  background: "#17191c",
+  white: "#fff",
+  gray: "#aaa",
+};
+const lineHeight = {
+  paragraph: 2,
+  subTitle: 1.1428571429,
+  tagline: 1.7777777778,
+};
+
+export default theme = {
+  fontSize,
+  space,
+  color,
+  lineHeight,
+};
+
+// fontSize guidelines
+// 10px = 0.625
+// 13px = 0.813
+// 16px = 1rem
+// 18px = 1.125
+// 24px = 1.5 (mobile)
+// 30px = 1.875
+// 34px = 2.125
+// 44px = 2.75 (mobile)
+// 50px = 3.125
