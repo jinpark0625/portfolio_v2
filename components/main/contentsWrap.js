87b1f074058f3e553e55062c4cf46aa70879c832
jin park <jinpark0625@gmail.com>
2022-10-07 16:11:18 -0400

    Changed scroll pages & scroll ranges

--- components/main/contentsWrap.js
+++ components/main/contentsWrap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Scroll, useCursor, useScroll, Center } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useBlock } from "../project/blocks";
 import state from "./mainStore";
 import { Vector2, Vector3, MathUtils } from "three";
@@ -34,92 +34,76 @@ const ContentsWrap = ({ children, ...props }) => {
   }, []);
 
   useFrame(({ mouse, camera, clock }, delta) => {
+    // Elapsed Time
+    state.point.current.material.uniforms.uTime.value = clock.getElapsedTime();
+
     /**
      * mouse events
      */
-    state.point.current.material.uniforms.uTime.value = clock.getElapsedTime();
-
     const x = (mouse.x * canvasWidth) / 2;
     const y = (mouse.y * canvasHeight) / 2;
     if (hoveredRef.current) {
       state.point.current.material.uniforms.uMouse.value = new Vector2(x, y);
       state.point.current.material.uniforms.uMouseTrigger.value = 1;
     } else state.point.current.material.uniforms.uMouseTrigger.value = 0;
+
     /**
-     * scroll events
+     * Scroll events
      */
     // scene - 1
-    const aScroll = scroll.range(0, 1 / 7);
-    state.point.current.material.uniforms.uRandom.value = aScroll;
-    const a = scroll.visible(0, 0.7 / 7);
-    const b = scroll.visible(0.7 / 7, 1 / 7);
+    const aScroll = scroll.range(0, 1 / 10);
+    const a = scroll.visible(0, 0.7 / 10);
+    const b = scroll.visible(0.7 / 10, 1 / 10);
+    const bRange = scroll.range(0.7 / 10, 1 / 10);
 
+    state.point.current.material.uniforms.uFirstTrigger.value = aScroll;
     a && setCurrentBodyIndex(null);
-    if (b) {
-      setCurrentBodyIndex(0);
-      state.point.current.material.uniforms.uTrigger.value = 0;
-    }
+    b && setCurrentBodyIndex(0);
+    state.point.current.material.uniforms.uLowerOpacity.value = bRange;
 
     //  scene - 2
-    const c = scroll.visible(1.7 / 7, 1 / 7);
-    const d = scroll.visible(2.7 / 7, 0.6 / 7);
-    const e = scroll.visible(3.2 / 7, 1.4 / 7);
-    const f = scroll.visible(3.6 / 7, 1 / 7);
+    const c = scroll.visible(1.7 / 10, 1 / 10);
+    const d = scroll.visible(2.7 / 10, 0.6 / 10);
+    const e = scroll.visible(3.2 / 10, 1.4 / 10);
+    const f = scroll.visible(3.6 / 10, 1 / 10);
 
-    const cRange = scroll.range(1.7 / 7, 1 / 7);
-    const cOpacity = scroll.range(1.7 / 7, 0.55 / 7);
-    const dOpacity = scroll.range(2.7 / 7, 0.8 / 7);
+    const cRange = scroll.range(1.7 / 10, 1 / 10);
+    const fRange = scroll.range(3.6 / 10, 1 / 10);
 
-    if (c) {
-      setCurrentBodyIndex(null);
-      state.point.current.material.uniforms.uOpacity.value = cOpacity;
-    }
-    state.point.current.material.uniforms.uTrigger.value = cRange;
-    if (e) {
-      // state.point.current.material.uniforms.uRandomSecond.value = dOpacity;
-      setCurrentBodyIndex(null);
-    }
+    c && setCurrentBodyIndex(null);
+    state.point.current.material.uniforms.uSecondTrigger.value = cRange;
+    state.point.current.material.uniforms.uIncreaseOpacity.value = cRange;
+
+    e && setCurrentBodyIndex(null);
+
     if (f) {
       setCurrentBodyIndex(1);
+      state.point.current.material.uniforms.uLowerOpacity.value = fRange + 0.97;
     }
 
     // scene - 3
-    const g = scroll.visible(4.6 / 7, 1 / 7);
-    const gRange = scroll.range(4.6 / 7, 1 / 7);
-    g && setCurrentBodyIndex(null);
-    state.point.current.material.uniforms.uTriggerTwo.value = gRange;
+    const g = scroll.visible(4.6 / 10, 1 / 10);
+    const h = scroll.visible(5.6 / 10, 1 / 10);
+    const gRange = scroll.range(4.6 / 10, 1 / 10);
+
+    if (g) {
+      setCurrentBodyIndex(null);
+      state.point.current.material.uniforms.uIncreaseOpacity.value = gRange;
+    }
+    state.point.current.material.uniforms.uThirdTrigger.value = gRange;
+    h && setCurrentBodyIndex(2);
 
     // scene - 4
-    const h = scroll.visible(5.6 / 7, 1 / 7);
-    const hRange = scroll.range(5.6 / 7, 1 / 7);
-    state.point.current.material.uniforms.uTriggerThree.value = hRange;
-    state.point.current.material.uniforms.uRandomThird.value = hRange;
-    // const f = scroll.visible(3.55 / 7, 1 / 7);
-    // const fRange = scroll.range(3.55 / 7, 1 / 7);
-    // const g = scroll.visible(4.55 / 7, 0.7 / 7);
-    // const gRange = scroll.range(4.55 / 7, 0.7 / 7);
-    // const h = scroll.visible(5.25 / 7, 1 / 7);
-    // const testNum = Number(eRange.toFixed(2));
-    // const testNumTwo = Number(gRange.toFixed(2));
+    const i = scroll.visible(6.6 / 10, 1 / 10);
+    const j = scroll.visible(7.6 / 10, 1 / 10);
+    const iRange = scroll.range(6.6 / 10, 1 / 10);
+    const jRange = scroll.range(7.6 / 10, 1 / 10);
+    state.point.current.material.uniforms.uFourthTrigger.value = iRange;
+    i && setCurrentBodyIndex(null);
 
-    // if (e) {
-    //   setCurrentBodyIndex(null);
-    //   ref.current.material.uniforms.uTrigger.value = testNum;
-    //   ref.current.material.uniforms.uOpacity.value = eOpacity;
-    //   ref.current.material.uniforms.uRandomSecond.value = 0;
-    // }
-    // if (f) {
-    //   ref.current.material.uniforms.uTrigger.value = 1;
-    //   ref.current.material.uniforms.uRandomSecond.value = fRange;
-    //   ref.current.material.uniforms.uTriggerTwo.value = 0;
-    // }
-    // if (g) {
-    //   ref.current.material.uniforms.uRandomSecond.value = 1;
-    //   ref.current.material.uniforms.uTriggerTwo.value = testNumTwo;
-    // }
-    // if (h) {
-    //   ref.current.material.uniforms.uTriggerTwo.value = 1;
-    // }
+    // const hRange = scroll.range(5.6 / 7, 1 / 7);
+    // state.point.current.material.uniforms.uTriggerThree.value = hRange;
+    state.point.current.geometry.verticesNeedUpdate = true;
   });
 
   return (
@@ -140,12 +124,6 @@ const ContentsWrap = ({ children, ...props }) => {
       <group>
         {children}
         <Center>
-          {/* <BodyText         
-          body="let's make a dot today."
-          index="0"
-          currentBodyIndex="0"
-          /> */}
-
           {bodyCentents.map((body, index) => (
             <BodyText
               key={index}
