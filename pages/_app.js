ad82b4d94c2023a2e2f4f58a64ca68cd70fd16e1
jin park <jinpark0625@gmail.com>
2022-10-11 22:10:12 -0400

    Add nProgress

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import Layout from "../components/layout";
+import { useEffect } from "react";
 import "../styles/globals.css";
+import "../styles/nprogress.css";
 import "../public/fonts/font.css";
+import Router from "next/router";
+import nProgress from "nprogress";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteStart = () => nProgress.start();
+    const handleRouteDone = () => nProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
