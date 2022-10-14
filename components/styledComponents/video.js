e66d1e64c827635d333d3b55f2f645bf58e4f694
jin park <jinpark0625@gmail.com>
2022-10-14 00:14:25 -0400

    Fixed video autoplay error in safari

--- components/styledComponents/video.js
+++ components/styledComponents/video.js
@@ -5,19 +5,33 @@ const VideoPlayer = ({ source, scale, poster }) => {
 
   useEffect(() => {
     if (videoRef && videoRef.current) {
+      videoRef.current.setAttribute("muted", "");
+      videoRef.current.defaultMuted = true;
+
       videoRef.current.play();
     }
   }, [videoRef]);
 
   return (
-    <video
-      ref={videoRef}
-      style={{ width: "100%", transform: `scaleX(${scale ? scale : "1.004"})` }}
-      src={source}
-      alt="Projects Video"
-      loop
-      muted
-      poster={poster}
+    <div
+      style={{
+        width: "100%",
+      }}
+      dangerouslySetInnerHTML={{
+        __html: `
+        <video
+          loop
+          muted
+          autoplay
+          playsinline
+          preload="metadata"
+          ref=${videoRef}
+          style="width: 100%; transform: scaleX(${scale ? scale : "1.004"});"
+          poster=${poster}
+        >
+        <source src="${source}" type="video/mp4" />
+        </video>`,
+      }}
     />
   );
 };
