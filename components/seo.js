bd476d116650fc59b253d47cc826ac635b1908e1
jin park <jinpark0625@gmail.com>
2022-10-14 00:59:39 -0400

    Add favicon images

--- components/seo.js
+++ components/seo.js
@@ -3,6 +3,27 @@ import Head from "next/head";
 const Seo = ({ title }) => {
   return (
     <Head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000" />
+      <meta name="msapplication-TileColor" content="#000" />
+      <meta name="theme-color" content="#ffffff" />
       <title>{title} | Jin Park</title>
     </Head>
   );
