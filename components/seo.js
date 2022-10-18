8bfec20f0ba19caf836857f95fcd45eb5db8ca0a
jin park <jinpark0625@gmail.com>
2022-10-18 17:50:01 -0400

    Add meta tags

--- components/seo.js
+++ components/seo.js
@@ -25,6 +25,18 @@ const Seo = ({ title }) => {
       <meta name="msapplication-TileColor" content="#000" />
       <meta name="theme-color" content="#ffffff" />
       <title>{title} | Jin Park</title>
+      <meta name="description" content="Jin Park's portfolio" />
+      <meta
+        name="keywords"
+        content="Jin,Jin Park, Portfolio, HTML, CSS, JavaScript, React, Next.js, Frontend"
+      />
+      <meta name="author" content="Jin Park" />
+      <meta name="og:site_name" content="Jin Park's portfolio" />
+      <meta name="og:title" content={`${title} | Jin Park`} />
+      <meta name="og:description" content="Jin Park's portfolio" />
+      <meta name="og:type" content="website" />
+      <meta name="og:url" content="https://portfolio-jinpark0625.vercel.app/" />
+      <meta name="og:image" content="/android-chrome-512x512.png" />
     </Head>
   );
 };
