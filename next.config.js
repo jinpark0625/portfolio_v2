3838af4df04e6e828821a0c7e252d015988d2d04
jin park <jinpark0625@gmail.com>
2022-10-18 22:12:07 -0400

    Delete unnecessary files

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withPlugins = require("next-compose-plugins");
-
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const withCss = require("@zeit/next-css");
-const withPurgeCss = require("next-purgecss");
-
-// module.exports = withBundleAnalyzer({});
-
-module.exports = withPlugins([
-  [withCss(withPurgeCss())],
-  [withBundleAnalyzer, {}],
-]);
+module.exports = withBundleAnalyzer({});
