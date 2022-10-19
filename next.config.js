d552f403c18a3b85995d6535f033e5c8cd1ad8dc
jin park <jinpark0625@gmail.com>
2022-10-19 00:40:36 -0400

    Add compression-webpack & fixed transition in mobile

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 
+const withPlugins = require("next-compose-plugins");
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({});
+const CompressionPlugin = require("compression-webpack-plugin");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(new CompressionPlugin());
+    return config;
+  },
+};
+
+module.exports = withPlugins(
+  [
+    withBundleAnalyzer({
+      compress: true,
+    }),
+  ],
+  nextConfig
+);
