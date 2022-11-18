34471d40f9b21fef3405f2567839f4ae199c2e07
jin park <jinpark0625@gmail.com>
2022-11-17 23:10:49 -0500

    Combine smoothscrolling with scrollprogress

--- components/project/scrollSmooth.js
+++ components/project/scrollSmooth.js
@@ -9,7 +9,7 @@ import {
 } from "framer-motion";
 import ResizeObserver from "resize-observer-polyfill";
 
-const ScrollSmooth = ({ mainColor, subColor, children }) => {
+const ScrollSmooth = ({ mainColor, subColor, pointColor, children }) => {
   const { scrollYProgress, scrollY } = useScroll();
   // scroll container
   const scrollRef = useRef(null);
@@ -35,11 +35,19 @@ const ScrollSmooth = ({ mainColor, subColor, children }) => {
   const physics = { damping: 15, mass: 0.27, stiffness: 55 }; // easing of smooth scroll
   const spring = useSpring(transform, physics); // apply easing to the negative scroll value
 
+  // set points to change background
   const backgroundFramer = useTransform(
     scrollYProgress,
     [0.2, 0.3, 0.8, 0.9],
     [mainColor, subColor, subColor, mainColor]
   );
+
+  // progress of scroll
+  const scaleY = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
   return (
     <LazyMotion features={domAnimation}>
@@ -50,6 +58,10 @@ const ScrollSmooth = ({ mainColor, subColor, children }) => {
       >
         {children}
       </m.div>
+      <m.div
+        className="progress_bar"
+        style={{ scaleY, background: pointColor }}
+      />
       <div style={{ height: pageHeight }} />
     </LazyMotion>
   );
