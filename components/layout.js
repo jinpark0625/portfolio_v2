ec4c8717f2c67e9e1ba7ecdd285d901528c77992
jin park <jinpark0625@gmail.com>
2022-11-17 22:51:06 -0500

    Remove react-spring && Add framer-motion for page transition

--- components/layout.js
+++ components/layout.js
@@ -1,45 +1,48 @@
 import Navbar from "./navBar";
-import { useRouter } from "next/router";
-import { motion, AnimatePresence } from "framer-motion";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
+import { AnimatePresence, domAnimation, LazyMotion, m } from "framer-motion";
+import { useState, memo } from "react";
 
-function Layout({ children }) {
-  const { asPath } = useRouter();
-  const variants = {
-    in: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.65,
-      },
+const variants = {
+  in: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.65,
     },
-    out: {
-      opacity: 0,
-      y: 40,
-      transition: {
-        duration: 0.65,
-      },
+  },
+  out: {
+    opacity: 0,
+    y: 40,
+    transition: {
+      duration: 0.65,
     },
-  };
+  },
+};
+
+function Layout({ asPath, children }) {
+  const [exitBefore, setExitBefore] = useState(false);
 
   return (
     <ThemeProvider theme={theme}>
       <Navbar />
-
-      <AnimatePresence initial={false} exitBeforeEnter>
-        <motion.div
-          key={asPath}
-          variants={variants}
-          animate="in"
-          initial="out"
-          exit="out"
-          className="container"
-        >
-          {children}
-        </motion.div>
-      </AnimatePresence>
+      {/* page transition */}
+      <LazyMotion features={domAnimation}>
+        <AnimatePresence exitBeforeEnter={!exitBefore}>
+          <m.div
+            key={asPath}
+            className="container"
+            animate="in"
+            initial="out"
+            exit="out"
+            variants={variants}
+          >
+            {children}
+          </m.div>
+        </AnimatePresence>
+      </LazyMotion>
     </ThemeProvider>
   );
 }
-export default Layout;
+export default memo(Layout);
