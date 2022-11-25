89ec8c6dc302aace9d269d249360921d1e3026ba
jin park <jinpark0625@gmail.com>
2022-11-25 01:06:01 -0500

    Simplify codes

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,13 @@
-import { Tagline, Block } from "../components/styledComponents";
 import Seo from "../components/seo";
+import { LostWay } from "../components/styledComponents/commonStyles";
 
 const NotFound = () => {
   return (
     <>
       <Seo title="To Infinity and Beyond" />
-      <Block
-        width="100%"
-        height="100vh"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Tagline fontColor="#fff">I think you are lost.</Tagline>
-      </Block>
+      <LostWay>
+        <div className="text">I think you are lost.</div>
+      </LostWay>
     </>
   );
 };
