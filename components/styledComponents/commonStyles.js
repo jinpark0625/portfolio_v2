d67fa5086068c766679bc773e2ff0c4852db9c4b
jin park <jinpark0625@gmail.com>
2022-11-29 16:03:39 -0500

    Add footer styles

--- components/styledComponents/commonStyles.js
+++ components/styledComponents/commonStyles.js
@@ -12,3 +12,50 @@ export const LostWay = styled.div`
     font-size: ${({ theme: { fontSize } }) => fontSize.tagline}rem;
   }
 `;
+
+export const ProjectFooter = styled.div`
+  height: auto;
+
+  .footer_container {
+    height: 100vh;
+    z-index: 1;
+  }
+
+  .footer_empty_height {
+    height: 100vh;
+  }
+
+  .footer_image_wrap {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .footer_text_container {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    color: #fff;
+    text-align: center;
+    transform: translate(-50%, -50%);
+  }
+
+  .footer_text_wrap {
+    position: relative;
+    width: 100%;
+  }
+
+  .footer_percentage {
+    position: absolute;
+    right: -24px;
+    top: -10px;
+  }
+
+  .footer_title {
+    font-family: SomeTimes;
+    font-size: 1.875rem;
+    margin-bottom: 12px;
+  }
+`;
