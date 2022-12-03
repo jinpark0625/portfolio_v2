26089ac309e4464ab797b4f8c0d0412094198818
jin park <jinpark0625@gmail.com>
2022-12-03 01:50:56 -0500

    Add binWorks images

--- components/project/images.js
+++ components/project/images.js
@@ -1,8 +1,10 @@
 // Work Page
 import YbWork from "/public/images/proejct_2.webp";
+import BwWork from "/public/images/proejct_4.webp";
 
 // Next Image
 import CoNext from "/public/images/next/coffee_next.webp";
+import FoNext from "/public/images/next/four_next.webp";
 
 // Yellow Basket
 import Ybimg1 from "/public/images/yellowbasket/yellow_img_1.webp";
@@ -14,9 +16,21 @@ import YbimgM1 from "/public/images/yellowbasket/yellow_img_m_1.webp";
 import YbimgM2 from "/public/images/yellowbasket/yellow_img_m_2.webp";
 import YbimgM3 from "/public/images/yellowbasket/yellow_img_m_3.webp";
 
+// Bin Works
+import Bwimg1 from "/public/images/binworks/binworks_img_1.webp";
+import Bwimg2 from "/public/images/binworks/binworks_img_2.webp";
+import Bwimg3 from "/public/images/binworks/binworks_img_3.webp";
+import Bwimg4 from "/public/images/binworks/binworks_img_4.webp";
+import Bwimg5 from "/public/images/binworks/binworks_img_5.webp";
+import BwimgM1 from "/public/images/binworks/binworks_img_m_1.webp";
+import BwimgM2 from "/public/images/binworks/binworks_img_m_2.webp";
+import BwimgM3 from "/public/images/binworks/binworks_img_m_3.webp";
+
 export {
   YbWork,
+  BwWork,
   CoNext,
+  FoNext,
   Ybimg1,
   Ybimg2,
   Ybimg3,
@@ -25,4 +39,12 @@ export {
   YbimgM1,
   YbimgM2,
   YbimgM3,
+  Bwimg1,
+  Bwimg2,
+  Bwimg3,
+  Bwimg4,
+  Bwimg5,
+  BwimgM1,
+  BwimgM2,
+  BwimgM3,
 };
