7074e000fdfe1e41d16e7705690b99713b23bafd
jin park <jinpark0625@gmail.com>
2022-12-03 01:32:06 -0500

    Add images.js to import all images

---
+++ components/project/images.js
@@ -0,0 +1,28 @@
+// Work Page
+import YbWork from "/public/images/proejct_2.webp";
+
+// Next Image
+import CoNext from "/public/images/next/coffee_next.webp";
+
+// Yellow Basket
+import Ybimg1 from "/public/images/yellowbasket/yellow_img_1.webp";
+import Ybimg2 from "/public/images/yellowbasket/yellow_img_2.webp";
+import Ybimg3 from "/public/images/yellowbasket/yellow_img_3.webp";
+import Ybimg4 from "/public/images/yellowbasket/yellow_img_4.webp";
+import Ybimg5 from "/public/images/yellowbasket/yellow_img_5.webp";
+import YbimgM1 from "/public/images/yellowbasket/yellow_img_m_1.webp";
+import YbimgM2 from "/public/images/yellowbasket/yellow_img_m_2.webp";
+import YbimgM3 from "/public/images/yellowbasket/yellow_img_m_3.webp";
+
+export {
+  YbWork,
+  CoNext,
+  Ybimg1,
+  Ybimg2,
+  Ybimg3,
+  Ybimg4,
+  Ybimg5,
+  YbimgM1,
+  YbimgM2,
+  YbimgM3,
+};
