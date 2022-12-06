90c960370ce5c490e4b5ae46211b30b5630b9b6f
jin park <jinpark0625@gmail.com>
2022-12-05 23:12:02 -0500

    Import images

--- components/project/images.js
+++ components/project/images.js
@@ -1,10 +1,24 @@
 // Work Page
 import YbWork from "/public/images/proejct_2.webp";
+import CoWork from "/public/images/proejct_3.webp";
 import BwWork from "/public/images/proejct_4.webp";
+import RcWork from "/public/images/proejct_6.webp";
 
 // Next Image
+import GcNext from "/public/images/next/cafe_next.webp";
 import CoNext from "/public/images/next/coffee_next.webp";
 import FoNext from "/public/images/next/four_next.webp";
+
+// Good cafeteria
+import Gcimg1 from "/public/images/cafeteria/cafeteria_img_1.webp";
+import Gcimg2 from "/public/images/cafeteria/cafeteria_img_2.webp";
+import Gcimg3 from "/public/images/cafeteria/cafeteria_img_3.webp";
+import Gcimg4 from "/public/images/cafeteria/cafeteria_img_4.webp";
+import Gcimg5 from "/public/images/cafeteria/cafeteria_img_5.webp";
+import Gcimg6 from "/public/images/cafeteria/cafeteria_img_6.webp";
+import Gcimg7 from "/public/images/cafeteria/cafeteria_img_7.webp";
+import Gcimg8 from "/public/images/cafeteria/cafeteria_img_8.webp";
+import Gcimg9 from "/public/images/cafeteria/cafeteria_img_9.webp";
 
 // Yellow Basket
 import Ybimg1 from "/public/images/yellowbasket/yellow_img_1.webp";
@@ -26,11 +40,68 @@ import BwimgM1 from "/public/images/binworks/binworks_img_m_1.webp";
 import BwimgM2 from "/public/images/binworks/binworks_img_m_2.webp";
 import BwimgM3 from "/public/images/binworks/binworks_img_m_3.webp";
 
+// Coffee Bak
+import Coimg1 from "/public/images/coffeebak/coffee_img_1.webp";
+import Coimg2 from "/public/images/coffeebak/coffee_img_2.webp";
+import Coimg3 from "/public/images/coffeebak/coffee_img_3.webp";
+import Coimg4 from "/public/images/coffeebak/coffee_img_4.webp";
+import CoimgM1 from "/public/images/coffeebak/coffee_img_m_1.webp";
+import CoimgM2 from "/public/images/coffeebak/coffee_img_m_2.webp";
+import CoimgM3 from "/public/images/coffeebak/coffee_img_m_3.webp";
+import CoimgM4 from "/public/images/coffeebak/coffee_img_m_4.webp";
+import CoimgM5 from "/public/images/coffeebak/coffee_img_m_5.webp";
+import CoimgM6 from "/public/images/coffeebak/coffee_img_m_6.webp";
+import CoimgM7 from "/public/images/coffeebak/coffee_img_m_7.webp";
+import CoimgM8 from "/public/images/coffeebak/coffee_img_m_8.webp";
+import CoimgM9 from "/public/images/coffeebak/coffee_img_m_9.webp";
+import CoimgM10 from "/public/images/coffeebak/coffee_img_m_10.webp";
+import CoimgM11 from "/public/images/coffeebak/coffee_img_m_11.webp";
+import CoimgM12 from "/public/images/coffeebak/coffee_img_m_12.webp";
+
+// four toon
+import Foimg1 from "/public/images/fourtoon/fourtoon_img_1.webp";
+import Foimg2 from "/public/images/fourtoon/fourtoon_img_2.webp";
+import Foimg3 from "/public/images/fourtoon/fourtoon_img_3.webp";
+import FoimgM1 from "/public/images/fourtoon/fourtoon_img_m_1.webp";
+import FoimgM2 from "/public/images/fourtoon/fourtoon_img_m_2.webp";
+import FoimgM3 from "/public/images/fourtoon/fourtoon_img_m_3.webp";
+import FoimgM4 from "/public/images/fourtoon/fourtoon_img_m_4.webp";
+import FoimgM5 from "/public/images/fourtoon/fourtoon_img_m_5.webp";
+import FoimgM6 from "/public/images/fourtoon/fourtoon_img_m_6.webp";
+import FoimgM7 from "/public/images/fourtoon/fourtoon_img_m_7.webp";
+import FoimgM8 from "/public/images/fourtoon/fourtoon_img_m_8.webp";
+import FoimgM9 from "/public/images/fourtoon/fourtoon_img_m_9.webp";
+import FoimgM10 from "/public/images/fourtoon/fourtoon_img_m_10.webp";
+import FoimgM11 from "/public/images/fourtoon/fourtoon_img_m_11.webp";
+import FoimgM12 from "/public/images/fourtoon/fourtoon_img_m_12.webp";
+
+// rent car
+import Rcimg1 from "/public/images/rentcar/rent_img_1.webp";
+import Rcimg2 from "/public/images/rentcar/rent_img_2.webp";
+import Rcimg3 from "/public/images/rentcar/rent_img_3.webp";
+import Rcimg4 from "/public/images/rentcar/rent_img_4.webp";
+import Rcimg5 from "/public/images/rentcar/rent_img_5.webp";
+import RcimgM1 from "/public/images/rentcar/rent_img_m_1.webp";
+import RcimgM2 from "/public/images/rentcar/rent_img_m_2.webp";
+import RcimgM3 from "/public/images/rentcar/rent_img_m_3.webp";
+
 export {
   YbWork,
+  CoWork,
   BwWork,
+  RcWork,
+  GcNext,
   CoNext,
   FoNext,
+  Gcimg1,
+  Gcimg2,
+  Gcimg3,
+  Gcimg4,
+  Gcimg5,
+  Gcimg6,
+  Gcimg7,
+  Gcimg8,
+  Gcimg9,
   Ybimg1,
   Ybimg2,
   Ybimg3,
@@ -47,4 +118,43 @@ export {
   BwimgM1,
   BwimgM2,
   BwimgM3,
+  Coimg1,
+  Coimg2,
+  Coimg3,
+  Coimg4,
+  CoimgM1,
+  CoimgM2,
+  CoimgM3,
+  CoimgM4,
+  CoimgM5,
+  CoimgM6,
+  CoimgM7,
+  CoimgM8,
+  CoimgM9,
+  CoimgM10,
+  CoimgM11,
+  CoimgM12,
+  Foimg1,
+  Foimg2,
+  Foimg3,
+  FoimgM1,
+  FoimgM2,
+  FoimgM3,
+  FoimgM4,
+  FoimgM5,
+  FoimgM6,
+  FoimgM7,
+  FoimgM8,
+  FoimgM9,
+  FoimgM10,
+  FoimgM11,
+  FoimgM12,
+  Rcimg1,
+  Rcimg2,
+  Rcimg3,
+  Rcimg4,
+  Rcimg5,
+  RcimgM1,
+  RcimgM2,
+  RcimgM3,
 };
