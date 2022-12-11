31c839e5f1941560028317e8cafd7d2844afa9f8
jin park <jinpark0625@gmail.com>
2022-12-11 02:38:14 -0500

    Set border-raius to mobile images

--- components/project/projectStyles.js
+++ components/project/projectStyles.js
@@ -212,11 +212,16 @@ export const ImageContainer = styled.div`
     overflow: hidden;
     width: 100%;
     height: 100%;
+    border-radius: 12px;
   }
 
   @media screen and (max-width: ${({ theme: { mediaQuery } }) =>
       mediaQuery.mobile}px) {
     margin-top: ${({ marginTop }) => (marginTop ? "32px" : "unset")};
+
+    .image_wrap {
+      border-radius: 4px;
+    }
   }
 `;
 
