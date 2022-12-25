8b561d29d2299d0b0eaeaed798600b0e17297b9b
jin park <jinpark0625@gmail.com>
2022-12-25 01:16:51 -0500

    Add simulationVertexShader

---
+++ components/shaders/simulationVertexShader.js
@@ -0,0 +1,19 @@
+const vertexShader = `
+varying vec2 vUv;
+
+void main() {
+  vUv = uv;
+
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectedPosition;
+
+}
+
+`;
+
+export default vertexShader;
+
+// simulation vertexShader에는 따른 data를 받지않는다.
