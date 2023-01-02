a3068c3540fc62c80ed98a104d2651bd7016cc71
jin park <jinpark0625@gmail.com>
2023-01-02 14:23:03 -0500

    Turn off perf

--- pages/work/index.js
+++ pages/work/index.js
@@ -3,6 +3,7 @@ import "../../components/project/projectsShader";
 import Seo from "../../components/seo";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
+// import { Perf } from "r3f-perf";
 
 const ProjectContentWrap = dynamic(
   () => import("../../components/project/projectContentWrap"),
@@ -22,8 +23,10 @@ const Work = () => {
         linear
         orthographic
         frameloop="demand"
+        gl={{ antialias: false }}
         camera={{ zoom: 75, position: [0, 0, 500] }}
       >
+        {/* <Perf deepAnalyze showGraph matrixUpdate /> */}
         <ProjectContentWrap router={router} />
       </Canvas>
     </div>
