fa3a5523242edf34e3175f41b7fa6e092fcdb1fb
jin park <jinpark0625@gmail.com>
2023-01-02 14:19:29 -0500

    Removed mainconentsWrap & Add scene component

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,14 @@
 import Seo from "../components/seo";
-import { Canvas } from "@react-three/fiber";
 import { useRouter } from "next/router";
-import { isMobile } from "react-device-detect";
-import dynamic from "next/dynamic";
-
-const MainContentsWrap = dynamic(
-  () => import("../components/main/mainContentsWrap"),
-  {
-    ssr: false,
-  }
-);
+import Scene from "../components/main/scene";
 
 export default function Home() {
   const router = useRouter();
 
-  const _isMobile = isMobile;
-
   return (
     <div className="canvasWrap" style={{ cursor: "auto" }}>
       <Seo title="Home" />
-      <Canvas
-        camera={{
-          fov: 75,
-          near: 0.1,
-          far: 10,
-        }}
-        className="canvas"
-        gl={{ antialias: false }}
-        dpr={[1, 2]}
-      >
-        <MainContentsWrap router={router} isMobile={_isMobile} />
-      </Canvas>
+      <Scene router={router} />
     </div>
   );
 }
