ebfe7863d02e19376656445da5830c3f68616f6e
jin park <jinpark0625@gmail.com>
2023-01-02 14:09:54 -0500

    Fixed conditional statements for models

--- components/shaders/meshSampler.js
+++ components/shaders/meshSampler.js
@@ -1,16 +1,16 @@
 import { MeshSurfaceSampler } from "three-stdlib";
 import { Vector3 } from "three/src/math/Vector3.js";
 
-const MeshSampler = (width, height, geometry, viewport, modelType) => {
+const MeshSampler = (width, height, geometry, viewportWidth, modelType) => {
   let length = width * height * 4;
-  let data = new Float32Array(length);
-  let peopleData = new Float32Array(((width * height) / 20) * 4);
 
   const sampler = new MeshSurfaceSampler(geometry);
   sampler.build();
   const tempPosition = new Vector3();
 
   if (modelType === 1) {
+    let data = new Float32Array(length);
+
     for (let i = 0; i < length; i++) {
       const stride = i * 4;
       sampler.sample(tempPosition);
@@ -22,6 +22,8 @@ const MeshSampler = (width, height, geometry, viewport, modelType) => {
     }
     return data;
   } else if (modelType === 2) {
+    let peopleData = new Float32Array(((width * height) / 20) * 4);
+
     for (let i = 0; i < length; i++) {
       const stride = i * 4;
       sampler.sample(tempPosition);
@@ -32,13 +34,15 @@ const MeshSampler = (width, height, geometry, viewport, modelType) => {
     }
     return peopleData;
   } else {
+    let data = new Float32Array(length);
+
     for (let i = 0; i < length; i++) {
       const stride = i * 4;
       sampler.sample(tempPosition);
 
-      data[stride] = tempPosition.x * (viewport.width / 18);
-      data[stride + 1] = tempPosition.y * (viewport.width / 18);
-      data[stride + 2] = tempPosition.z * (viewport.width / 18);
+      data[stride] = tempPosition.x * (viewportWidth / 18);
+      data[stride + 1] = tempPosition.y * (viewportWidth / 18);
+      data[stride + 2] = tempPosition.z * (viewportWidth / 18);
       data[stride + 3] = 1.0;
     }
     return data;
