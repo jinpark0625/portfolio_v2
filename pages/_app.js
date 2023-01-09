8027784ad5f85ee09af802197145b9dd368c3da8
jin park <jinpark0625@gmail.com>
2023-01-09 15:21:57 -0500

    Improve performance by refactoring state management with useReducer hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Layout from "../components/layout";
-import { useEffect, useLayoutEffect } from "react";
+import { useLayoutEffect, useReducer, useCallback } from "react";
 import "../styles/globals.min.css";
 import "../styles/nprogress.min.css";
 import "../public/fonts/font.css";
@@ -7,36 +7,56 @@ import Router, { useRouter } from "next/router";
 import nProgress from "nprogress";
 import { Analytics } from "@vercel/analytics/react";
 
-export default function App({ Component, pageProps }) {
-  // nProgress
-  useEffect(() => {
-    const handleRouteStart = () => nProgress.start();
-    const handleRouteDone = () => nProgress.done();
+const nProgressReducer = (state, action) => {
+  switch (action.type) {
+    case "start":
+      nProgress.start();
+      return { ...state, isLoading: true };
+    case "done":
+      nProgress.done();
+      return { ...state, isLoading: false };
+    default:
+      throw new Error("Cannot implement nProgress");
+  }
+};
 
+export default function App({ Component, pageProps }) {
+  const [nProgressState, dispatch] = useReducer(nProgressReducer, {
+    isLoading: false,
+  });
+
+  const handleRouteStart = useCallback(() => {
+    dispatch({ type: "start" });
+  }, []);
+
+  const handleRouteDone = useCallback(() => {
+    dispatch({ type: "done" });
+  }, []);
+
+  useLayoutEffect(() => {
     Router.events.on("routeChangeStart", handleRouteStart);
     Router.events.on("routeChangeComplete", handleRouteDone);
     Router.events.on("routeChangeError", handleRouteDone);
 
     return () => {
-      // Make sure to remove the event handler on unmount!
       Router.events.off("routeChangeStart", handleRouteStart);
       Router.events.off("routeChangeComplete", handleRouteDone);
       Router.events.off("routeChangeError", handleRouteDone);
     };
-  }, []);
+  }, [handleRouteStart, handleRouteDone]);
 
   // size for chrome
-  const handleResize = () => {
+  const handleResize = useCallback(() => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
-  };
+  }, []);
 
   useLayoutEffect(() => {
     handleResize();
     window.addEventListener("resize", handleResize);
 
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [handleResize]);
 
   const { asPath } = useRouter();
 
