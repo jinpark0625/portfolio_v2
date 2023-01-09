107740192b4d76cf7a5fdde802bbbbee97fbfc0d
jin park <jinpark0625@gmail.com>
2023-01-09 16:21:02 -0500

    Improve performance of rendering large lists

--- pages/about.js
+++ pages/about.js
@@ -7,8 +7,129 @@ import {
   AboutEducation,
   AboutWork,
 } from "../components/styledComponents/aboutStyles";
+import { memo, useMemo } from "react";
+
+const Skills = memo(function Skills({ className, title, skills }) {
+  return (
+    <div className={className}>
+      <p className="list_title">{title}</p>
+      <ul className="list_text">
+        {skills.map((skill) => (
+          <li key={skill}>{skill}</li>
+        ))}
+      </ul>
+    </div>
+  );
+});
+
+const Education = memo(function Education({
+  className,
+  education1,
+  education2,
+  education3,
+}) {
+  return (
+    <div className="list">
+      <p className={className}>{education1}</p>
+      <p className={`margin_m ${className}`}>{education2}</p>
+      <p className={`margin_m ${className}`}>{education3}</p>
+    </div>
+  );
+});
+
+const EducationM = memo(function EducationM({
+  className,
+  education1,
+  education2,
+}) {
+  return (
+    <div className={`edu_item_m ${className}`}>
+      <p className="list_title">{education1}</p>
+      <p className="list_text">{education2}</p>
+    </div>
+  );
+});
+
+const Work = memo(function Work({ title, date, position, descriptions }) {
+  return (
+    <div className="list_wrap">
+      <div className="list">
+        <p className="list_title">{title}</p>
+        <div className="small_text margin_s">{date}</div>
+        <div className="small_text">{position}</div>
+      </div>
+      <div className="work_item_des">
+        <ul>
+          {descriptions.map((description) => (
+            <li className="work_list" key={description}>
+              {description}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+});
 
 const About = () => {
+  const skills = useMemo(() => {
+    return {
+      language: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Three.js",
+        "Next.js",
+        "TypeScript",
+      ],
+      application: [
+        "Photoshop",
+        "Illustrator",
+        "AdobeXd",
+        "Figma",
+        "Indesign",
+        "Premiere",
+      ],
+      others: [
+        "Bootstrap",
+        "Tailwind CSS",
+        "Styled Components",
+        "Storybook",
+        "Redux",
+        "MobX",
+      ],
+    };
+  }, []);
+
+  const description = useMemo(() => {
+    return {
+      dMonster: [
+        "Developed front-end code for 7 websites and Worked closely with clients to meet project requirements, goals, and desired functionality.",
+        "Implemented mobile applications using React Native.",
+        "Collaborated with the design team to design new features for websites and mobile applications that increased user engagement by 40%.",
+      ],
+      tomato: [
+        "Designed visual content for Facebook and Instagram posts that increased followers by 200%.",
+        "Collaborated with the design team to create advertising posters and videos.",
+      ],
+      hanmi: [
+        "Designed packages like delivery boxes, poly bags, and packing tape to elevate and differentiate branding.",
+        "Developed creative advertising content for various media,including catalogs, business cards, flyers, posters, and product packing while ensuring maximum brand awareness of the target audience.",
+      ],
+      cheum: [
+        "Designed, and created branding collateral, such as itineraries, letters, and catalogs.",
+      ],
+      north: [
+        "Conceptualized and created their logo, and designed Twitter Banners and Twitch Panels.",
+      ],
+      chicago: [
+        "Perform installation and removal of window presentations and in-store displays in support of promotional events for new product launches.",
+        "Coordinate with the visual design manager to develop and implement visual displays.",
+      ],
+    };
+  }, []);
+
   return (
     <>
       <Seo title="About" />
@@ -51,40 +172,17 @@ const About = () => {
         <AboutSkill className="wrap">
           <div className="subtitle">SKILLS</div>
           <div className="list_wrap">
-            <div className="list">
-              <p className="list_title">Languages & Libraries</p>
-              <ul className="list_text">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Three.js</li>
-                <li>Next.js</li>
-                <li>TypeScript</li>
-              </ul>
-            </div>
-            <div className="list">
-              <p className="list_title">Applications</p>
-              <ul className="list_text">
-                <li>Photoshop</li>
-                <li>Illustrator</li>
-                <li>AdobeXd</li>
-                <li>Figma</li>
-                <li>Indesign</li>
-                <li>Premiere</li>
-              </ul>
-            </div>
-            <div className="list">
-              <p className="list_title">Others</p>
-              <ul className="list_text">
-                <li>Bootstrap</li>
-                <li>Tailwind CSS</li>
-                <li>Styled Components</li>
-                <li>Storybook</li>
-                <li>Redux</li>
-                <li>MobX</li>
-              </ul>
-            </div>
+            <Skills
+              className="list"
+              title="Languages & Libraries"
+              skills={skills.language}
+            />
+            <Skills
+              className="list"
+              title="Applications"
+              skills={skills.application}
+            />
+            <Skills className="list" title="Others" skills={skills.others} />
           </div>
         </AboutSkill>
 
@@ -92,154 +190,80 @@ const About = () => {
         <AboutEducation className="wrap">
           <p className="subtitle">EDUCATION</p>
           <div className="list_wrap">
-            <div className="list">
-              <p className="list_title">Vom Academy</p>
-              <p className="margin_m list_title">Seneca College</p>
-              <p className="margin_m list_title">
-                Kyungpook National University
-              </p>
-            </div>
-            <div className="list">
-              <p className="list_text">Front End Developer Bootcamp</p>
-              <p className="margin_m list_text">Creative Advertising</p>
-              <p className="margin_m list_text">Fashion Design</p>
-            </div>
-            <div className="edu_item_m">
-              <p className="list_title">Vom Academy</p>
-              <p className="list_text">Front End Developer Bootcamp</p>
-            </div>
-            <div className="edu_item_m margin_m">
-              <p className="list_title">Seneca College</p>
-              <p className="list_text">Creative Advertising</p>
-            </div>
-            <div className="edu_item_m margin_m">
-              <p className="list_title">Kyungpook National University</p>
-              <p className="list_text">Fashion Design</p>
-            </div>
+            <Education
+              className="list_title"
+              education1="Vom Academy"
+              education2="Seneca College"
+              education3="Kyungpook National University"
+            />
+            <Education
+              className="list_text"
+              education1="Front End Developer Bootcamp"
+              education2="Creative Advertising"
+              education3="Fashion Design"
+            />
+            <EducationM
+              education1="Vom Academy"
+              education2="Front End Developer Bootcamp"
+            />
+            <EducationM
+              className="margin_m"
+              education1="Seneca College"
+              education2="Creative Advertising"
+            />
+            <EducationM
+              className="margin_m"
+              education1="Kyungpook National University"
+              education2="Fashion Design"
+            />
           </div>
         </AboutEducation>
 
         {/* work */}
         <AboutWork className="wrap">
           <div className="subtitle">WORK</div>
-          <div className="list_wrap">
-            <div className="list">
-              <p className="list_title">DMonster</p>
-              <div className="small_text margin_s">2021-2022</div>
-              <div className="small_text">Junior Frontend Developer</div>
-            </div>
 
-            <div className="work_item_des">
-              <ul>
-                <li className="work_list">
-                  Developed front-end code for 7 websites and Worked closely
-                  with clients to meet project requirements, goals, and desired
-                  functionality.
-                </li>
-                <li className="work_list">
-                  Implemented mobile applications using React Native.
-                </li>
-                <li className="work_list">
-                  Collaborated with the design team to design new features for
-                  websites and mobile applications that increased user
-                  engagement by 40%.
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="list_wrap">
-            <div className="list">
-              <p className="list_title">Tomato Project</p>
-              <div className="small_text margin_s">2019-Present</div>
-              <div className="small_text">Graphic Designer (Volunteering)</div>
-            </div>
-            <div className="work_item_des">
-              <ul>
-                <li className="work_list">
-                  Designed visual content for Facebook and Instagram posts that
-                  increased followers by 200%.
-                </li>
-                <li className="work_list">
-                  Collaborated with the design team to create advertising
-                  posters and videos.
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="list_wrap">
-            <div className="list">
-              <p className="list_title">Hanmi Post</p>
-              <div className="small_text margin_s">2019</div>
-              <div className="small_text">Graphic Designer</div>
-            </div>
-            <div className="work_item_des">
-              <ul>
-                <li className="work_list">
-                  Designed packages like delivery boxes, poly bags, and packing
-                  tape to elevate and differentiate branding.
-                </li>
-                <li className="work_list">
-                  Developed creative advertising content for various media,
-                  including catalogs, business cards, flyers, posters, and
-                  product packing while ensuring maximum brand awareness of the
-                  target audience.
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="list_wrap">
-            <div className="list">
-              <p className="list_title">Cheum Tour</p>
-              <div className="small_text margin_s">2019</div>
-              <div className="small_text">Freelance Graphic Designer</div>
-            </div>
+          <Work
+            title="DMonster"
+            date="2021-2022"
+            position="Junior Frontend Developer"
+            descriptions={description.dMonster}
+          />
 
-            <div className="work_item_des">
-              <ul>
-                <li className="work_list">
-                  Designed, and created branding collateral, such as
-                  itineraries, letters, and catalogs.
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="list_wrap">
-            <div className="list">
-              <p className="list_title">Northern Esports Academy</p>
-              <div className="small_text margin_s">2019</div>
-              <div className="small_text">Freelance Graphic Designer</div>
-            </div>
-            <div className="work_item_des">
-              <ul>
-                <li className="work_list">
-                  Conceptualized and created their logo, and designed Twitter
-                  Banners and Twitch Panels.
-                </li>
-              </ul>
-            </div>
-          </div>
+          <Work
+            title="Tomato Project"
+            date="2019-Present"
+            position="Graphic Designer (Volunteering)"
+            descriptions={description.tomato}
+          />
 
-          <div className="list_wrap">
-            <div className="list">
-              <p className="list_title">Chicago City Sports</p>
+          <Work
+            title="Hanmi Post"
+            date="2019"
+            position="Graphic Designer"
+            descriptions={description.hanmi}
+          />
 
-              <div className="small_text margin_s">2015-2016</div>
-              <div className="small_text">Internship (Visual Merchandiser)</div>
-            </div>
-            <div className="work_item_des">
-              <ul>
-                <li className="work_list">
-                  Perform installation and removal of window presentations and
-                  in-store displays in support of promotional events for new
-                  product launches.
-                </li>
-                <li className="work_list">
-                  Coordinate with the visual design manager to develop and
-                  implement visual displays.
-                </li>
-              </ul>
-            </div>
-          </div>
+          <Work
+            title="Cheum Tour"
+            date="2019"
+            position="Freelance Graphic Designer"
+            descriptions={description.cheum}
+          />
+
+          <Work
+            title="Northern Esports Academy"
+            date="2019"
+            position="Freelance Graphic Designer"
+            descriptions={description.north}
+          />
+
+          <Work
+            title="Chicago City Sports"
+            date="2015-2016"
+            position="Internship (Visual Merchandiser)"
+            descriptions={description.chicago}
+          />
         </AboutWork>
       </AboutContainer>
     </>
