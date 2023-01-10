8f2d3aec91c36012fbf598af0e791eb0168f6080
jin park <jinpark0625@gmail.com>
2023-01-10 15:58:27 -0500

    Fixed meta url

--- components/seo.js
+++ components/seo.js
@@ -28,14 +28,14 @@ const Seo = ({ title }) => {
       <meta name="description" content="Jin Park's portfolio" />
       <meta
         name="keywords"
-        content="Jin,Jin Park, Portfolio, HTML, CSS, JavaScript, React, Next.js, Frontend"
+        content="Jin, Jin Park, Portfolio, HTML, CSS, JavaScript, React, Next.js, Frontend"
       />
       <meta name="author" content="Jin Park" />
       <meta name="og:site_name" content="Jin Park's portfolio" />
       <meta name="og:title" content={`${title} | Jin Park`} />
       <meta name="og:description" content="Jin Park's portfolio" />
       <meta name="og:type" content="website" />
-      <meta name="og:url" content="https://portfolio-jinpark0625.vercel.app/" />
+      <meta name="og:url" content="https://www.parkjin.dev/" />
       <meta name="og:image" content="/android-chrome-512x512.png" />
     </Head>
   );
