7e1a29ec4e033eda951f3207065b1c76314cc7c5
jin park <jinpark0625@gmail.com>
2023-01-12 18:05:33 -0500

    Add lazy loader component

---
+++ components/lazyLoader.js
@@ -0,0 +1,10 @@
+const LazyLoader = () => {
+  return (
+    <div className="lazy_loading_container">
+      <div className="lazy_loading_bar" />
+      <div className="loading-data">Loading 0%</div>
+    </div>
+  );
+};
+
+export default LazyLoader;
