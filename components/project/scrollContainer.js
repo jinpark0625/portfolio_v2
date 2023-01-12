8d030342bacc1aa3cecf039533e29b964ec6c5b1
jin park <jinpark0625@gmail.com>
2023-01-12 17:57:25 -0500

    Splitted scrollContainer.js into smaller chunks using dynamic imports

---
+++ components/project/scrollContainer.js
@@ -0,0 +1,83 @@
+import { useState, useLayoutEffect, useCallback, useRef } from "react";
+import { useScroll, LazyMotion, domAnimation } from "framer-motion";
+import ResizeObserver from "resize-observer-polyfill";
+import SmoothScroll from "./smoothScroll";
+import dynamic from "next/dynamic";
+import { m, useSpring, useTransform } from "framer-motion";
+
+const ScrollBar = dynamic(() => import("./scrollBar"));
+const ProjectFooter = dynamic(() => import("./projectFooter"));
+
+const ScrollContainer = ({
+  mainColor,
+  subColor,
+  pointColor,
+  nextProject,
+  link,
+  nextImage,
+  children,
+}) => {
+  const scrollRef = useRef();
+
+  const { scrollYProgress, scrollY } = useScroll();
+
+  // page scrollable height based on content length
+  const [pageHeight, setPageHeight] = useState(0);
+
+  // update scrollable height when browser is resizing
+  const resizePageHeight = useCallback((entries) => {
+    for (let entry of entries) {
+      setPageHeight(entry.contentRect.height);
+    }
+  }, []);
+
+  // observe when browser is resizing
+  useLayoutEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) =>
+      resizePageHeight(entries)
+    );
+    scrollRef && resizeObserver.observe(scrollRef.current);
+    return () => resizeObserver.disconnect();
+  }, [scrollRef, resizePageHeight]);
+
+  return (
+    <LazyMotion features={domAnimation}>
+      {/* smooth scroll */}
+      <SmoothScroll
+        mainColor={mainColor}
+        subColor={subColor}
+        pageHeight={pageHeight}
+        scrollY={scrollY}
+        scrollYProgress={scrollYProgress}
+        ref={scrollRef}
+        m={m}
+        useSpring={useSpring}
+        useTransform={useTransform}
+      >
+        {children}
+      </SmoothScroll>
+
+      {/* scroll progress bar */}
+      <ScrollBar
+        pointColor={pointColor}
+        scrollYProgress={scrollYProgress}
+        m={m}
+        useSpring={useSpring}
+      />
+      {/* the basis for content height  */}
+      <div style={{ height: pageHeight }} />
+
+      {/* footer */}
+      <ProjectFooter
+        nextImage={nextImage}
+        scrollY={scrollY}
+        nextProject={nextProject}
+        link={link}
+        m={m}
+        useTransform={useTransform}
+      />
+    </LazyMotion>
+  );
+};
+
+export default ScrollContainer;
