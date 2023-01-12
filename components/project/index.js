c3481d5a2aca4f4e471ed1c8113a5ae39ce82fc4
jin park <jinpark0625@gmail.com>
2023-01-12 17:50:58 -0500

    Remove scrollContainer

--- components/project/index.js
+++ components/project/index.js
@@ -1,6 +1,5 @@
 import { Block, useBlock } from "./blocks";
 import state from "./scrollStore";
-import ScrollSmooth from "./scrollSmooth";
 import ProjectPages from "./projectPages";
 
-export { Block, useBlock, state, ScrollSmooth, ProjectPages };
+export { Block, useBlock, state, ProjectPages };
